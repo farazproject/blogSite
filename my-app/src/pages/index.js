ff4a63e3920121e2a6ec49838a1ce81c23e898df
farazproject <muhammad.gulfaraz@live.com>
2025-05-29 16:02:48 +0500

    component count for state change

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,3 +1,8 @@
+import React from "react";
+import Counter from "@/components/count/count";
+
+
+
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import List from "@/components/list/list";
@@ -17,7 +22,8 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <List/>
+      {/* <List/> */}
+      <Counter/>
 
     </div>
   );
