6ef69d89af606330874cd477efca8ef3677c18c3
farazproject <muhammad.gulfaraz@live.com>
2025-06-11 15:34:24 +0500

    update

--- my-app/src/pages/posts/[id]/index.js
+++ my-app/src/pages/posts/[id]/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useParams } from 'next/navigation'
 
 function PostDetail() {
+
+    const Params = useParams();
+    console.log(Params);
+
+
+
+
   return (
     <div>index</div>
   )
